5470ad26b97507f619da6995ef05e64375c1cb10
misha-pelykh <michaelpelykh@gmail.com>
2018-04-07 21:36:03 +0300

    add the registration ability for server user

--- app/models/server_user.rb
+++ app/models/server_user.rb
@@ -1,5 +1,6 @@
 class ServerUser < ApplicationRecord
-  devise :database_authenticatable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable,
+         :registerable, :confirmable
 
   has_many :oauth_applications, class_name: 'Doorkeeper::Application', as: :owner
 end
