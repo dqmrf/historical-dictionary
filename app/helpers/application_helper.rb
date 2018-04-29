c7d5f586b23c725f9ee516b39e81cf0265b67060
misha-pelykh <michaelpelykh@gmail.com>
2018-04-29 14:38:01 +0300

    add flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def bootstrap_alert_name(key)
+    case key.to_sym
+      when :notice then 'info'
+      when :success then 'success'
+      when :alert then 'warning'
+      when :error then 'danger'
+      else 'info'
+    end
+  end
 end
